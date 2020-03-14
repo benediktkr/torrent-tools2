c4866844bee2590c8773f376a2c6429f8c622984
Benedikt Kristinsson <benedikt@lokun.is>
2020-03-14 19:14:15 +0000

    works now

--- tt-sort/sort.py
+++ tt-sort/sort.py
@@ -5,10 +5,14 @@ import shutil
 import os
 
 from guessit import guessit
+from colorama import Fore, Style
+
+Yellow = Fore.YELLOW
+Green = Fore.GREEN
+RESET_ALL = Style.RESET_ALL
 
 
-
-def sort(src, dst, cp, dry_run=False):
+def sort(src, dst, cp, dry_run=False, verbose=False):
     titles = {'episode': set(), 'movie': set()}
     ignored = set()
     qs = ['720p', '1080p']
@@ -32,20 +36,25 @@ def sort(src, dst, cp, dry_run=False):
                         if guess_basename['title'].title() == title:
                             # This means we're moving the entire parent dir
                             # so there's no need to continue with the rest
-                            move(root, dst, d['type'], title, cp, dry_run)
+                            r = move(root, dst, d['type'], title, cp, dry_run)
+                            if verbose:
+                                print("{}: {}".format(r, root.split("/")[-1]))
+
                             break
                         else:
                             # We are moving a singular file, so we need to
                             # continue in the loop (no break)
                             src = os.path.join(root, f)
-                            move(src, dst, d['type'], title, cp, dry_run)
+                            r = move(src, dst, d['type'], title, cp, dry_run)
+                            if verbose:
+                                print("{}: {}".format(r, src.split("/")[-1]))
                     else:
                         ignored.add(f)
             except KeyError:
                 ignored.add(f)
 
 
-    if args.debug:
+    if verbose:
         print("Ignored files:")
         for ign in ignored:
             print("* {}".format(ign))
@@ -63,31 +72,38 @@ def move(src, dst, type, title, cp=False, dry_run=False):
     if type == "episode":
         dst = os.path.join(dst, title)
 
-    if args.debug:
-        print("{} -> {}/".format(src, dst))
-
     os.makedirs(dst, exist_ok=True)
+
+    src2 = src.split('/')[-1]
     if not dry_run:
         if cp:
-            if os.path.exists(dst):
-                if args.debug:
-                    print("skipping: {}".format(dst))
-            else:
-                shutil.copyfile(src, dst)
+            if os.path.exists(os.path.join(dst, src2)):
+                return Yellow + "skipped" + RESET_ALL
+            # copy2 preserves metadata
+            try:
+                shutil.copy2(src, dst)
+                return Green + "copied" + RESET_ALL
+            except IsADirectoryError:
+                #return Yellow + "skipped" + RESET_ALL
+                shutil.copytree(src, os.path.join(dst, src2))
+                return Green + "copied" + RESET_ALL
         else:
             shutil.move(src, dst)
+            return Green + "moved" + RESET_ALL
 
-    # touch the basename to simulate some moving
-    import pathlib
-    pathlib.Path(os.path.join(dst, os.path.basename(src))).touch()
+    if dry_run:
+        # touch the basename to simulate some moving
+        import pathlib
+        pathlib.Path(os.path.join(dst, os.path.basename(src))).touch()
+        return "dry_run"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("src")
     parser.add_argument("dst")
-    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("--cp", action="store_true")
     parser.add_argument("--dry-run", action="store_true")
     args = parser.parse_args()
 
-    sort(args.src, args.dst, args.cp, args.dry_run)
+    sort(args.src, args.dst, args.cp, args.dry_run, args.verbose)
